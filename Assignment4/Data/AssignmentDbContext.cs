0178cd469dda34123029f115fd601a5cc02848ad
mohdhassan7890@gmail.com <mohdhassan7890@gmail.com>
2023-09-14 23:49:06 +0530

    First commit

---
+++ Assignment4/Data/AssignmentDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Assignment4.Models;
+
+namespace Assignment4.Data
+{
+    public class AssignmentDbContext : DbContext
+    {
+        public AssignmentDbContext (DbContextOptions<AssignmentDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Assignment4.Models.Student> Student { get; set; } = default!;
+    }
+}
---
+++ Assignment4/Models/Student.cs
@@ -0,0 +1,10 @@
+﻿namespace Assignment4.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Class { get; set; }
+        public int Age { get; set; }
+    }
+}
